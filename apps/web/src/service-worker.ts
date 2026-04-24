47e5f64bfd0a36bbbac6f4757e6a3660c64dc4c1
espen <espen.erlandsen@gmail.com>
2026-04-24 17:32:57 +0200

    Revert "chore(web): auto-bump version after merge #710"
    This reverts commit d55b07ac9d796c9f2afb5f7b9d56943e4ed9ab8f.

--- apps/web/src/service-worker.ts
+++ apps/web/src/service-worker.ts
@@ -5,7 +5,7 @@
 
 import { build, files, version } from "$service-worker";
 
-const CACHE_VERSION = "325";
+const CACHE_VERSION = "324";
 const CACHE = `cache-${version}-${CACHE_VERSION}`;
 
 const ASSETS = [
