4a7c9ec8bd18f17d8789ed7d6f8c131e166c0398
Espen Erlandsen <espen.erlandsen@gmail.com>
2026-09-22 00:20:06 +0200

    ✨ feat(answers): add system-neutral political-intrigue running guide (#3262) (#3263)

--- apps/web/src/lib/content/answers/pages/how-do-you-create-a-fantasy-faction.ts
+++ apps/web/src/lib/content/answers/pages/how-do-you-create-a-fantasy-faction.ts
@@ -158,6 +158,7 @@ export const howDoYouCreateAFantasyFaction: AnswerConfigInput = {
     "how-do-you-run-a-scene-with-multiple-npcs",
     "how-do-i-expand-a-simple-rpg-campaign-idea",
     "what-rpg-works-for-political-intrigue-and-faction-play",
+    "how-do-i-run-political-intrigue-and-faction-play",
   ],
   discovery: {
     id: "answer-fantasy-faction",
